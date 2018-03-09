89e9f73e1b99a4532a14ad79d430cfb18261fdfb
Charlie Phung <phunghiep2709@gamil.com>
2018-03-09 14:06:59 +0200

    adding validation

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
- // Material Select Initialization
- $(document).ready(function() {
-    $('.mdb-select').material_select();
+$(document).ready(function () {
+  $('#modalDefaultContactForm').modal('show');
+});
+
+$(document).ready(function () {
+  var msg = "";
+  var elements = document.getElementsByClassName("form-input");
+  var button = document.getElementById("submit");
+
+  console.log(elements);
+
+
+  button.addEventListener("click", () => {
+    for (let i = 0; i < elements.length; i++) {
+      if (elements[i].validity.valueMissing) {
+        elements[i].placeholder = "Vui lòng điền thông tin!";
+      }
+    }
   });
+
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].validity.valueMissing) {
+      elements[i].addEventListener("focus", () => {
+        elements[i].placeholder = "";
+      })
+    }
+  }
+});
